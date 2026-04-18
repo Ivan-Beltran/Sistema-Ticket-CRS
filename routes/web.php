c1387eb4a07648e20e51697694abdef1d99c7bfb
Merlyn8989 <merlynrajo@gmail.com>
2026-04-18 11:02:39 -0600

    ruta modificada

--- routes/web.php
+++ routes/web.php
@@ -45,12 +45,8 @@ Route::middleware(['auth'])->group(function () {
         });
     });
 
-    Route::get('/tickets', [TicketController::class, 'index'])->name('tickets.index');
-    Route::get('/tickets/create', [TicketController::class, 'create'])->name('tickets.create');
-    Route::post('/tickets', [TicketController::class, 'store'])->name('tickets.store');
+   Route::resource('tickets', TicketController::class);
 
-
-    Route::get('/tickets/{ticket}', [TicketController::class, 'show'])->name('tickets.show');
     // Rutas de SLA Plans
     Route::resource('/sla-plans',  SlaPlanController::class);
     // Rutas de prioridades
